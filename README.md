2db8bd5fdc9e36eb5e4285b80a0c238106a2d629
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-11-07 13:46:28 -0800

    added readme:

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # weather-api
+
+## What does the WeatherAPI do?
+
+This web application allows the user to look up the name of the city and it will then generate the weather of that city, as well as the weather for the next 5 days
+
+We used Javascript, css, html, handlebars, node, api, and express to create this project
+
+## Motivation
+
+We wanted to create an application that was both functional and entertaining.
+
+## What we learned
+
+Creating this project we learned how other
+
+## Installation
+
+## Screenshots
+
+## Credits
+
+## License
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,127 @@
-//beginning variables
 function initPage() {
-const cityEl = document.getElementById("#city");
-const searchEl = document.getElementById(".search");
-const clearEl = document.getElementById("clear-history");
-const nameEl = document.getElementById("city-name");
-const currentPicEl = document.getElementById("current-pic");
-const currentTempEl = document.getElementById("temperature");
-const currentHumidityEl = document.getElementById("humidity");4
-const currentWindEl = document.getElementById("wind-speed");
-const currentUVEl = document.getElementById("UV-index");
-const historyEl = document.getElementById("history");
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-}
+    const cityEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
 
-//fetch request and console log for weather
-const apiKey = "2b5273ff9c8458a9e729503a2001b7f4";
+    const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+//  When search button is clicked, read the city name typed by the user
 
-const cityResult = userFormEl.value;
-console.log(cityResult);
+    function getWeather(cityName) {
+//  Using saved city name, execute a current condition get request from open weather map api
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        axios.get(queryURL)
+        .then(function(response){
+            console.log(response);
+//  Parse response to display current conditions
+        //  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+            const currentDate = new Date(response.data.dt*1000);
+            console.log(currentDate);
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth() + 1;
+            const year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt",response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+        let lat = response.data.coord.lat;
+        let lon = response.data.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+        axios.get(UVQueryURL)
+        .then(function(response){
+            let UVIndex = document.createElement("span");
+            UVIndex.setAttribute("class","badge badge-danger");
+            UVIndex.innerHTML = response.data[0].value;
+            currentUVEl.innerHTML = "UV Index: ";
+            currentUVEl.append(UVIndex);
+        });
+//  Using saved city name, execute a 5-day forecast get request from open weather map api
+        let cityID = response.data.id;
+        let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+        axios.get(forecastQueryURL)
+        .then(function(response){
+//  Parse response to display forecast for next 5 days underneath current conditions
+            console.log(response);
+            const forecastEls = document.querySelectorAll(".forecast");
+            for (i=0; i<forecastEls.length; i++) {
+                forecastEls[i].innerHTML = "";
+                const forecastIndex = i*8 + 4;
+                const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                const forecastDay = forecastDate.getDate();
+                const forecastMonth = forecastDate.getMonth() + 1;
+                const forecastYear = forecastDate.getFullYear();
+                const forecastDateEl = document.createElement("p");
+                forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
+                forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                forecastEls[i].append(forecastDateEl);
+                const forecastWeatherEl = document.createElement("img");
+                forecastWeatherEl.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                forecastWeatherEl.setAttribute("alt",response.data.list[forecastIndex].weather[0].description);
+                forecastEls[i].append(forecastWeatherEl);
+                const forecastTempEl = document.createElement("p");
+                forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
+                forecastEls[i].append(forecastTempEl);
+                const forecastHumidityEl = document.createElement("p");
+                forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                forecastEls[i].append(forecastHumidityEl);
+                }
+            })
+        });  
+    }
 
-const formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  const cityResult = userFormEl.value.trim();
-  findCity(cityResult);
-  console.log(cityResult);
-
-  const query =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=lat&lon=lon&exclude=part&appid=${apiKey}";
-
-  fetch(query)
-    .then((response) => response.json())
-    .then((data) => console.log("data: ", data));
-};
-
-
-//display weather
-const findBook = function (bookName) {
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookName}`)
-    // Converts the response to JSON
-    .then(function (response) {
-      return response.json();
+    searchEl.addEventListener("click",function() {
+        const searchTerm = cityEl.value;
+        getWeather(searchTerm);
+        searchHistory.push(searchTerm);
+        localStorage.setItem("search",JSON.stringify(searchHistory));
+        renderSearchHistory();
     })
-    .then(function (response) {
-      const listSearch = response.items;
-      //card block to display
-      let template = "";
-      listSearch.forEach((item) => {
-        template += `
-          <div class="card">
-              <div class="card-content">
-              <div class="media">
-                  <div class="media-left">
-                  <figure class="image is-48x48">
-                      <img src="${item.volumeInfo.imageLinks.smallThumbnail}" alt="Placeholder image">
-                  </figure>
-                  </div>
-                  <div class="media-content">
-                  <p class="title is-4"></p>
-                  <p class="subtitle is-6">${item.volumeInfo.authors}</p>
-                  </div>
-              </div>
-          
-              <div class="content">
-                  ${item.volumeInfo.description}
-              </div>
-              </div>
-          </div>
-          `;
 
-        // YOUR CODE HERE
-        document.querySelector("#results-container").innerHTML = template;
-      });
-    });
+    clearEl.addEventListener("click",function() {
+        searchHistory = [];
+        renderSearchHistory();
+    })
 
-  }
-searchBtn.addEventListener("click", formSubmitHandler);
+    function k2f(K) {
+        return Math.floor((K - 273.15) *1.8 +32);
+    }
+
+    function renderSearchHistory() {
+        historyEl.innerHTML = "";
+        for (let i=0; i<searchHistory.length; i++) {
+            const historyItem = document.createElement("input");
+            // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
+            historyItem.setAttribute("type","text");
+            historyItem.setAttribute("readonly",true);
+            historyItem.setAttribute("class", "form-control d-block bg-white");
+            historyItem.setAttribute("value", searchHistory[i]);
+            historyItem.addEventListener("click",function() {
+                getWeather(historyItem.value);
+            })
+            historyEl.append(historyItem);
+        }
+    }
+
+    renderSearchHistory();
+    if (searchHistory.length > 0) {
+        getWeather(searchHistory[searchHistory.length - 1]);
+    }
+
+
+//  Save user's search requests and display them underneath search form
+//  When page loads, automatically generate current conditions and 5-day forecast for the last city the user searched for
+
+}
+initPage();
